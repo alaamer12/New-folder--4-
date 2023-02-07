7d0e66692d7d4db011d1a91c6afd3916ad9289bc
amr2023 <amrmuhamed86@hotmail.com>
2023-02-07 09:57:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,119 @@
+document.querySelector("h1").style.color = "Blue";
+window.onload = function () {
+ document.querySelector("h1").style.color = "pink";
+};
+for (let m = 0; m < 10; m++) {
+ document.write("this js ");
+}
+let amr = "amr";
+let ahmed = "anoia";
+console.log(`${amr} is not
+${ahmed}`);
+console.log(45);
+console.log((44).toString());
+console.log((43).toString(8));
+console.log(parseInt("1am0r0"));
+console.log(Math.random());
+console.log(Math.random(5));
+20 > 50
+ ? console.log("first try")
+ : 25 > 50
+ ? console.log("2nd try")
+ : 60 > 50
+ ? console.log("yes its and this 3rd try")
+ : console.log("false");
+
+const u = 4444;
+
+// let arr1 =[1,2,10,4,5,6,7]
+// let arr2 = [,4,5,6,7,8,9]
+// let arr3 = ["a","b","c","d","e","f","g","h","i",]
+// console.log(`${arr1 + arr2}`)
+// console.log(arr1 + arr2)
+// mainLoop: for (let i=0; i<arr1.length; i++){console.log(arr1[i])
+// nestedLoop: for (let j=0; j<arr2.length; j++){console.log(arr2[j])
+// nestedLoop1: for (let k=0; k<arr3.length; k++){console.log(arr1[k])
+
+// }}}
+let x = ["13"];
+for (let i = 0; i < 20; i++) {
+ if (i == 12) {
+  continue;
+ }
+ console.log(`${i} ${"\n"}`);
+}
+function sti(sssd, y = 4, z) {}
+let xa = function cal() {
+ return x;
+};
+console.log(xa());
+console.log(typeof xa());
+console.log(Array.isArray(xa()));
+
+setTimeout(function () {
+ console.log("timeout");
+}, 2000);
+
+let tr2 = "<h1>its h1 tag </h1>";
+let tr3 = document.write("<h1>its tr3 tag </h1>");
+let ty1 = document.write("tEXT  ");
+//document.write(ty1)
+
+let ec = `${"doDDDD"}
+${"ds"}`;
+document.write(tr2);
+
+var global = 5;
+console.log(global);
+{
+ let global = 4;
+ console.log(window.global);
+}
+document;
+
+oi = 5;
+var oi;
+console.log('oi', oi)
+
+let xz = 010;
+console.log(oi);
+
+let dad = {
+ age: 20,
+ f_dad: function () {
+  return dad.age * 2;
+ },
+};
+console.log(dad.f_dad());
+let son = Object.create(dad);
+son.age = 40;
+console.log(son.f_dad());
+let sds = 5;
+let s5 = 4;
+let s4 = 4;
+let s21s = 44;
+let trr = 523;
+console.log(trr);
+
+document.querySelector().getAttribute;
+
+document.getElementById().hasAttribute();
+document.createTextNode().appendChild();
+let ttt = 5;
+console.log("ttt", ttt);
+
+let p = "phenomenon";
+let t = "treasure";
+
+
+const result= await add(2,3)
+
+result
+let res = await add(2.4,5)
+res
+const sts =await add(3,4)
+sts
+var po=await add(4,8)
+po
+
+
